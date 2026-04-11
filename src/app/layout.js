b39e32a8e8bf4f123c205f4050e6a46e57a8ef75
MuhoroDev-tony <muhoroanthonydev@gmail.com>
2026-04-11 14:58:28 +0300

    fix google verification

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,12 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+
 export const metadata = {
   title: "DJ Alekie 254 | Official Website",
   description: "Official website of DJ Alekie 254 - Book DJ services in Kenya.",
-    verification: {
-    google: "efWDTcpUxC1M5BN_3U7zE-QCRBp5BfaY8rahbV_pOh8" 
+  verification: {
+    google: "efWDTcpUxC1M5BN_3U7zE-QCRBp5BfaY8rahbV_pOh8"
   },
 };
 
@@ -18,11 +19,6 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
-export const metadata = {
-  title: "DJ Alekie 254",
-  description: "Official DJ Alekie 254 Website",
-};
 
 export default function RootLayout({ children }) {
   return (
